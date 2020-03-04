a027bbfb4a5876368e620f9abc20f899326fbea3
Eric-Lombardo <ejlanim8or@gmail.com>
2020-03-04 20:04:41 +0000

    1_queries completed

--- 1_queries/04_all_my_reservations.sql
+++ 1_queries/04_all_my_reservations.sql
@@ -1,26 +1,4 @@
-
-
--- SELECT properties.id,
---   title, 
---   cost_per_night, 
---   start_date, 
---   (SELECT avg(rating) AS average_rating FROM property_reviews JOIN properties ON properties.id = property_reviews.property_id)
--- FROM property_reviews
---   JOIN properties ON properties.id = property_reviews.property_id
---   JOIN reservations ON property_reviews.reservation_id = reservations.id
---   JOIN users ON property_reviews.guest_id = users.id
--- WHERE users.id = 1
--- GROUP BY properties.id, title, cost_per_night, start_date
--- ORDER BY start_date;
-
-
--- -- display average rating true
--- SELECT avg(rating) AS average_rating
--- FROM property_reviews
---   JOIN properties ON properties.id = property_reviews.property_id
--- WHERE properties.title = 'Apple barn';
-
-SELECT properties.id, title, cost_per_night, start_date, avg(rating) as average_rating
+SELECT properties.*, reservations.*, avg(rating) as average_rating
 FROM reservations
 JOIN properties ON reservations.property_id = properties.id
 JOIN property_reviews ON properties.id = property_reviews.property_id 
